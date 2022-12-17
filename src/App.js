3b8693f7e0482944e794a7ac3180756fcda8d2c7
Marina <likholetova.marina@gmail.com>
2022-12-17 10:28:53 +0200

    Initial contact componets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "@mui/material";
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import generateRandomNumber from "./utils/generateRandomNumber";
+import listHeaders from "./data/listData";
+import contactsDb from "./data/contactsData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            contacts: contactsDb || [],
+            isShowForm: false,
+        };
+    }
+
+    addContact = (firstName, lastName, phone) => {
+        this.setState((state) => ({
+            contacts: [
+                ...state.contacts,
+                {
+                    firstName,
+                    lastName,
+                    phone,
+                    id: generateRandomNumber(),
+                },
+            ],
+        }));
+    };
+
+    deleteContact = (id) => {
+        this.setState((state) => ({
+            contacts: state.contacts.filter((contact) => contact.id !== id),
+        }));
+    };
+
+    toggleShowForm = () => {
+        this.setState((state) => ({
+            isShowForm: !state.isShowForm,
+        }));
+    };
+
+    onShowForm = () => {
+        this.toggleShowForm();
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="title">Contacts</h1>
+                <ContactList
+                    list={this.state.contacts}
+                    listHeaders={listHeaders}
+                    onDeleteContact={this.deleteContact}
+                />
+                <Button
+                    variant="contained"
+                    disabled={this.state.isShowForm}
+                    onClick={this.onShowForm}
+                >
+                    Add Contact
+                </Button>
+                {this.state.isShowForm && (
+                    <ContactForm
+                        addContact={this.addContact}
+                        toggleShowForm={this.toggleShowForm}
+                    />
+                )}
+            </div>
+        );
+    }
 }
-
-export default App;
